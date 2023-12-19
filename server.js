6451174293d2a7dc95dcd7a37001b5f4204dd3d7
Abir Hussain Khan Abid <abirkhanrcc@gmail.com>
2023-12-19 15:08:21 +0600

    connect database and created user model

--- server.js
+++ server.js
@@ -2,8 +2,12 @@ const express = require("express");
 const dotenv = require("dotenv");
 dotenv.config();
 
+const connectDB = require("./config/db.js");
+
 const port = process.env.PORT || 8000;
 const app = express();
+// connect database
+connectDB();
 
 app.listen(port, () => {
   console.log(`server is listening on port ${port}`);
