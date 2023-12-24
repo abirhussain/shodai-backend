bbae495fa39baa60c2dc4ea69830d52486538162
Abir Hussain Khan Abid <abirkhanrcc@gmail.com>
2023-12-25 03:40:41 +0600

    feat: change password:

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -4,31 +4,31 @@ import CryptoJS from "crypto-js";
 import User from "./user.model";
 
 import {
-	tokenForVerify,
-	sendEmail,
-	signInToken,
+  tokenForVerify,
+  sendEmail,
+  signInToken,
 } from "../../config/core/services/auth";
 
 export default class UserController {
-	public async verifyEmailAddress(req: Request, res: Response) {
-		const email = req.body.email;
-		const name = req.body.name;
-		const isEmailExist = await User.findOne({ email });
-		if (isEmailExist) {
-			return res.status(403).send({
-				message: "Email Already Exist!!!",
-			});
-		} else {
-			const user = req.body;
-			const token = tokenForVerify(user);
-			const mailOptions = {
-				from: {
-					name: "Shodai",
-					address: process.env.SENDER_EMAIL,
-				},
-				to: `${email}`,
-				subject: "Verify Your Email",
-				html: `<h2>Hello ${name}</h2>
+  public async verifyEmailAddress(req: Request, res: Response) {
+    const email = req.body.email;
+    const name = req.body.name;
+    const isEmailExist = await User.findOne({ email });
+    if (isEmailExist) {
+      return res.status(403).send({
+        message: "Email Already Exist!!!",
+      });
+    } else {
+      const user = req.body;
+      const token = tokenForVerify(user);
+      const mailOptions = {
+        from: {
+          name: "Shodai",
+          address: process.env.SENDER_EMAIL,
+        },
+        to: `${email}`,
+        subject: "Verify Your Email",
+        html: `<h2>Hello ${name}</h2>
             <p>Verify your email address to complete the signup and login into your <strong>SHODAI</strong> account.</p>
       
               <p>This link will expire in <strong> 15 minute</strong>.</p>
@@ -42,153 +42,206 @@ export default class UserController {
               <p style="margin-bottom:0px;">Thank you</p>
               <strong>Shodai Team</strong>
                    `,
-			};
+      };
 
-			const message = "Please check your email to verify!";
-			sendEmail(mailOptions, res, message);
-		}
-	}
+      const message = "Please check your email to verify!";
+      sendEmail(mailOptions, res, message);
+    }
+  }
 
-	public async registerUser(req: Request, res: Response) {
-		const token: string = req.params.token;
-		const decodedToken: any = jwt.decode(token);
-		const { name, email, password } = decodedToken;
-		const user = await User.findOne({ email });
-		if (user) {
-			const token = signInToken(user);
-			return res.send({
-				token,
-				name: user.name,
-				email: user.email,
-				message: "Email Already Verified!!!",
-			});
-		}
+  public async registerUser(req: Request, res: Response) {
+    const token: string = req.params.token;
+    const decodedToken: any = jwt.decode(token);
+    const { name, email, password } = decodedToken;
+    const user = await User.findOne({ email });
+    if (user) {
+      const token = signInToken(user);
+      return res.send({
+        token,
+        name: user.name,
+        email: user.email,
+        message: "Email Already Verified!!!",
+      });
+    }
 
-		if (token) {
-			jwt.verify(
-				token,
-				process.env.JWT_SECRET_FOR_VERIFICATION || "",
-				(err: any, decoded: any) => {
-					if (err) {
-						return res.status(401).send({
-							message: "Token Expired, Please try again!",
-						});
-					} else {
-						const newUser = new User({
-							name,
-							email,
-							password
-						});
-						newUser.save();
-						const token = signInToken(newUser);
-						res.send({
-							token,
-							_id: newUser._id,
-							name: newUser.name,
-							email: newUser.email,
-							message: "Email Verified, Please Login Now!",
-						});
-					}
-				}
-			);
-		}
-	}
+    if (token) {
+      jwt.verify(
+        token,
+        process.env.JWT_SECRET_FOR_VERIFICATION || "",
+        (err: any, decoded: any) => {
+          if (err) {
+            return res.status(401).send({
+              message: "Token Expired, Please try again!",
+            });
+          } else {
+            const newUser = new User({
+              name,
+              email,
+              password,
+            });
+            newUser.save();
+            const token = signInToken(newUser);
+            res.send({
+              token,
+              _id: newUser._id,
+              name: newUser.name,
+              email: newUser.email,
+              message: "Email Verified, Please Login Now!",
+            });
+          }
+        }
+      );
+    }
+  }
 
-	public async loginUser(req: Request, res: Response) {
-		try {
-			const email = req.body.email;
-			const password = req.body.password;
-			const user = await User.findOne({ email }, { __v: 0, updatedAt: 0, createdAt: 0 });
+  public async loginUser(req: Request, res: Response) {
+    try {
+      const email = req.body.email;
+      const password = req.body.password;
+      const user = await User.findOne(
+        { email },
+        { __v: 0, updatedAt: 0, createdAt: 0 }
+      );
 
-			if (user) {
-				const decryptedPassword = CryptoJS.AES.decrypt(user.password, process.env.CRYPTO_SECRET || "");
-				if (decryptedPassword.toString(CryptoJS.enc.Utf8) !== password) {
-					res.status(401).send({ message: "Invalid Credentials" });
-				} else {
-					const token = signInToken(user);
-					res.send({
-						token,
-						_id: user._id,
-						name: user.name,
-						email: user.email,
-						address: user.address,
-						phone: user.phone,
-						image: user.image,
-					});
-				}
-			}
-		} catch (err: any) {
-			res.status(500).send({
-				message: err.message,
-			});
-		}
-	};
+      if (user) {
+        const decryptedPassword = CryptoJS.AES.decrypt(
+          user.password,
+          process.env.CRYPTO_SECRET || ""
+        );
+        if (decryptedPassword.toString(CryptoJS.enc.Utf8) !== password) {
+          res.status(401).send({ message: "Invalid Credentials" });
+        } else {
+          const token = signInToken(user);
+          res.send({
+            token,
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            address: user.address,
+            phone: user.phone,
+            image: user.image,
+          });
+        }
+      }
+    } catch (err: any) {
+      res.status(500).send({
+        message: err.message,
+      });
+    }
+  }
 
-	public async forgetPassword(req: Request, res: Response) {
-		const email = req.body.email
-		const user = await User.findOne({ email }, { __v: 0, updatedAt: 0, createdAt: 0 });
-		if (!user) {
-			return res.status(404).send({
-				message: "User doesn't exist",
-			});
-		} else {
-			const token = tokenForVerify(user);
-			const mailOptions = {
-				from: process.env.SENDER_EMAIL,
-				to: `${email}`,
-				subject: 'Password Reset',
-				html: `<h2>Hello ${user.name.toUpperCase()}</h2>
+  public async forgetPassword(req: Request, res: Response) {
+    const email = req.body.email;
+    const user = await User.findOne(
+      { email },
+      { __v: 0, updatedAt: 0, createdAt: 0 }
+    );
+    if (!user) {
+      return res.status(404).send({
+        message: "User doesn't exist",
+      });
+    } else {
+      const token = tokenForVerify(user);
+      const mailOptions = {
+        from: process.env.SENDER_EMAIL,
+        to: `${email}`,
+        subject: "Password Reset",
+        html: `<h2>Hello ${user.name.toUpperCase()}</h2>
 			<p>A request has been received to change the password for your <strong>Shodai</strong> account </p>
 
 			  <p>This link will expire in <strong> 15 minutes</strong>.</p>
 
 			  <p style="margin-bottom:20px;">Click this link to reset password</p>
 	  
-			  <a href=${process.env.FRONTEND_BASE_URL}/user/forget-password/${token} style="background:#22c55e;color:white;border:1px solid #22c55e; padding: 10px 15px; border-radius: 4px; text-decoration:none;">Reset Password</a>
+			  <a href=${
+          process.env.FRONTEND_BASE_URL
+        }/user/forget-password/${token} style="background:#22c55e;color:white;border:1px solid #22c55e; padding: 10px 15px; border-radius: 4px; text-decoration:none;">Reset Password</a>
 	  
 			  <p style="margin-top: 35px;">If you did not initiate this request, please contact us immediately at support@shodai.com</p>
 	  
 			  <p style="margin-bottom:0px;">Thank you</p>
 			  <strong>Shodai Team</strong>
 				   `,
-			};
+      };
 
-			const message = 'A passwork reset link has been sent to your email !!!';
-			sendEmail(mailOptions, res, message);
-		}
-	};
+      const message = "A passwork reset link has been sent to your email !!!";
+      sendEmail(mailOptions, res, message);
+    }
+  }
 
-	public async resetPassword(req: Request, res: Response) {
-		const token = req.body.token;
-		const newPassword = req.body.password;
-		if (token) {
-			jwt.verify(token, process.env.JWT_SECRET_FOR_VERIFICATION || "", async (err: any, decoded: any) => {
-				if (err) {
-					return res.status(401).send({
-						message: 'Token expired, please try again !!!',
-					});
-				} else {
-					const decodedToken: any = jwt.decode(token);
-					const { email } = decodedToken;
-					const user = await User.findOne({ email }, { __v: 0, updatedAt: 0, createdAt: 0 });
-					if (user) {
-						user.password = newPassword;
-						user.save();
-						res.status(200).send({
-							message: 'Password has been changed successfully !!!',
-						});
-					} else {
-						return res.status(404).send({
-							message: "User doesn't exist",
-						});
-					}
+  public async resetPassword(req: Request, res: Response) {
+    const token = req.body.token;
+    const newPassword = req.body.password;
+    if (token) {
+      jwt.verify(
+        token,
+        process.env.JWT_SECRET_FOR_VERIFICATION || "",
+        async (err: any, decoded: any) => {
+          if (err) {
+            return res.status(401).send({
+              message: "Token expired, please try again !!!",
+            });
+          } else {
+            const decodedToken: any = jwt.decode(token);
+            const { email } = decodedToken;
+            const user = await User.findOne(
+              { email },
+              { __v: 0, updatedAt: 0, createdAt: 0 }
+            );
+            if (user) {
+              user.password = newPassword;
+              user.save();
+              res.status(200).send({
+                message: "Password has been changed successfully !!!",
+              });
+            } else {
+              return res.status(404).send({
+                message: "User doesn't exist",
+              });
+            }
+          }
+        }
+      );
+    } else {
+      res.status(401).send({
+        message: "Token is missing !!!",
+      });
+    }
+  }
 
-				}
-			});
-		} else {
-			res.status(401).send({
-				message: "Token is missing !!!"
-			})
-		}
-	};
+  public async changePassword(req: Request, res: Response) {
+    try {
+      const email = req.body.email;
+      const oldPwd = req.body.oldPwd;
+      const newPwd = req.body.newPwd;
+      const user = await User.findOne(
+        { email },
+        { __v: 0, updatedAt: 0, createdAt: 0 }
+      );
+      if (user) {
+        const decryptedPassword = CryptoJS.AES.decrypt(
+          user.password,
+          process.env.CRYPTO_SECRET || ""
+        );
+        if (decryptedPassword.toString(CryptoJS.enc.Utf8) !== oldPwd) {
+          res.status(401).send({ message: "Invalid Credentials" });
+        } else {
+          user.password = newPwd;
+          await user.save();
+          res.status(200).send({
+            message: "The password or passphrase is changed.!!!",
+          });
+        }
+      } else {
+        res.status(500).send({
+          message:
+            "Change password failed. Check whether the user ID and oldPwd you provide is correct.",
+        });
+      }
+    } catch (err: any) {
+      res.status(500).send({
+        message: err.message,
+      });
+    }
+  }
 }
--- src/modules/user/user.route.ts
+++ src/modules/user/user.route.ts
@@ -3,31 +3,41 @@ import UserController from "./user.controller";
 import { verificationRequestLimit } from "../../config/core/services/verificationRequestLimit";
 
 export default class UserRoutes {
-	private router = Router();
-	private userController: UserController = new UserController();
+  private router = Router();
+  private userController: UserController = new UserController();
 
-	constructor() {
-		this.initRoutes();
-	}
+  constructor() {
+    this.initRoutes();
+  }
 
-	private initRoutes(): void {
-		//Email Verification
-		this.router.post(
-			"/users/verify-email",
-			verificationRequestLimit,
-			this.userController.verifyEmailAddress
-		);
+  private initRoutes(): void {
+    //Email Verification
+    this.router.post(
+      "/users/verify-email",
+      verificationRequestLimit,
+      this.userController.verifyEmailAddress
+    );
 
-		// User Registration
-		this.router.post("/users/register/:token", this.userController.registerUser);
+    // User Registration
+    this.router.post(
+      "/users/register/:token",
+      this.userController.registerUser
+    );
 
-		//user signin
-		this.router.post('/users/signin', this.userController.loginUser);
+    //user signin
+    this.router.post("/users/signin", this.userController.loginUser);
 
-		//forget-password
-		this.router.put('/forget-password', verificationRequestLimit, this.userController.forgetPassword);
+    //forget-password
+    this.router.put(
+      "/forget-password",
+      verificationRequestLimit,
+      this.userController.forgetPassword
+    );
 
-		// Reset Password
-		this.router.put('/reset-password', this.userController.resetPassword);
-	}
+    // Reset Password
+    this.router.put("/reset-password", this.userController.resetPassword);
+
+    //Change Password
+    this.router.post("/change-password", this.userController.changePassword);
+  }
 }
